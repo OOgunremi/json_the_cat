7994664eb5121a708bc7c4be1b6df6f7db12a5d6
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-03-17 22:55:51 -0600

    adding testing package

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,14 +3,14 @@ const breed = process.argv.slice(2);
 
 const fetchBreedDescription = function(breedName, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-  request(url, function (error, response, body) {
+  request(url, function(error, response, body) {
     if (error) {
       return callback(error, null);
     }
     const data = JSON.parse(body);
     const breedName = data[0];
     if (!breedName) {
-      const breedNameError = (`Breed name ${breed[0]} does not exist.`);
+      const breedNameError = (`Breed name ${breed} does not exist.`);
       callback(breedNameError, null);
     } else {
       callback(null, breedName.description);
