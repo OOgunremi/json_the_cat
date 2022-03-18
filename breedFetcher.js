d7195e15f438bf9792f0b4a083fe4c930db4a441
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-03-17 21:42:02 -0600

    added called back functions

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,21 @@
 const request = require("request");
-
 const breed = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-request(url, function(error, response, body) {
-  if (error) {
-    return console.log('Failed to request details:', error);
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, function (error, response, body) {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    const breedName = data[0];
+    if (!breedName) {
+      const breedNameError = (`Breed name ${breed[0]} does not exist.`);
+      callback(breedNameError, null);
+    } else {
+      callback(null, breedName.description);
+    }
   }
-  const data = JSON.parse(body);
-  const breedName = data[0];
-  if (!breedName) {
-    console.log(`Breed name ${breed[0]} does not exist.`);
-  } else {
-    console.log(breedName.description);
-  }
-}
-);
+  );
+};
+module.exports = { fetchBreedDescription };
